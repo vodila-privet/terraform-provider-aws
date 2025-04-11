f6c9bd211e9e2d4cbfb5d39693682828298673f8
alexander.pesetski <alexander.pesetski@welltech.com>
2025-04-11 10:02:45 +0200

    feat: allow notebook-al2-v4 as platform_identifier

--- internal/service/sagemaker/notebook_instance_test.go
+++ internal/service/sagemaker/notebook_instance_test.go
@@ -435,6 +435,13 @@ func TestAccSageMakerNotebookInstance_Platform_identifier(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "platform_identifier", "notebook-al2-v3"),
 				),
 			},
+            {
+				Config: testAccNotebookInstanceConfig_platformIdentifier(rName, "notebook-al2-v4"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNotebookInstanceExists(ctx, resourceName, &notebook),
+					resource.TestCheckResourceAttr(resourceName, "platform_identifier", "notebook-al2-v4"),
+				),
+			},
 		},
 	})
 }
